b039188b2358f444e430f42a79601e4599bf4453
Zhan <9824373@qq.com>
2019-07-14 23:03:30 +0800

    complete the heartDisease algorithm

--- HeartDisease_FlyAI/main.py
+++ HeartDisease_FlyAI/main.py
@@ -13,38 +13,76 @@ from net import Net
 from path import MODEL_PATH
 
 parser = argparse.ArgumentParser()
-parser.add_argument("-e", "--EPOCHS", default=1000, type=int, help="train epochs")
+parser.add_argument(
+    "-e",
+    "--EPOCHS",
+    default=1000,
+    type=int,
+    help="train epochs")
 parser.add_argument("-b", "--BATCH", default=32, type=int, help="batch size")
 args = parser.parse_args()
 
-def eval(preds_prob,y_test):
+
+def eval(preds_prob, y_test):
     preds = np.zeros(preds_prob.shape)
     preds[preds_prob >= 0.5] = 1
     train_accuracy = (preds == y_test).sum() / preds_prob.shape[0]
 
     return train_accuracy
 
+
 # 训练并评估模型
 data = Dataset()
-x_train, y_train, x_test, y_test = data.next_batch(68,32)  # 读取数据
+model = Model(data)
+
+x_train, y_train, x_test, y_test = data.get_all_processor_data()
 # read in data
-dtrain = xgb.DMatrix(x_train,label = y_train)
-dtest = xgb.DMatrix(x_test,label = y_test)
+dtrain = xgb.DMatrix(x_train, label=y_train)
+dtest = xgb.DMatrix(x_test, label=y_test)
 
 best_accuracy = 0
 # specify parameters via map
-param = {'max_depth': 5, 'eta':0.3, 'objective':'binary:logistic','verbosity':3 }
-num_round = 10
-# specify validations set to watch performance
+etas = [0.001, 0.01, 0.1, 0.3, 0.5, 1]
+max_depths = [2, 3, 5, 7, 10]
+lambdas = [0.001, 0.01, 0.1, 0.3, 0.5, 1, 7, 10, ]
+alphas = [0.001, 0.01, 0.1, 0.3, 0.5, 1, 7, 10, ]
+num_rounds = [2, 5, 7, 10]
+
+# etas = [0.001,0.5,]
+# max_depths = [2]
+# lambdas = [0.001,0.5, ]
+# alphas = [0.001]
+# num_rounds = [2, 7]
+
 watchlist = [(dtest, 'eval'), (dtrain, 'train')]
-bst = xgb.train(param, dtrain, num_round,watchlist,verbose_eval=True)
-# make prediction
-preds_prob = bst.predict(dtest)
 
-train_accuracy = eval(preds_prob, dtest.get_label())
+for eta in etas:
+    for max_depth in max_depths:
+        for lambda_2 in lambdas:
+            for alpha in alphas:
+                for num_round in num_rounds:
+                    param = {
+                        'max_depth': max_depth,
+                        'eta': eta,
+                        'lambda': lambda_2,
+                        'alpha': alpha,
+                        'objective': 'binary:logistic',
+                        'verbosity': 3}
 
-if train_accuracy > best_accuracy:
-    best_accuracy = train_accuracy
-    model = Model(bst)
-    model.save_model(bst, MODEL_PATH, overwrite=True)
-    print("step %d, best accuracy %g" % (0, best_accuracy))
+                    # specify validations set to watch performance
+                    bst = xgb.train(
+                        param,
+                        dtrain,
+                        num_round,
+                        watchlist,
+                        verbose_eval=True)
+                    preds_prob = bst.predict(dtest)
+                    train_accuracy = eval(preds_prob, dtest.get_label())
+
+                    if train_accuracy > best_accuracy:
+                        best_accuracy = train_accuracy
+                        model.save_model(bst, MODEL_PATH, overwrite=True)
+                        print(
+                            "parameters: eta: %g, max_depth: %g, lambda_2: %g, alpha: %g, num_round: %g." %
+                            (eta, max_depth, lambda_2, alpha, num_round))
+                        print("best accuracy %s" % best_accuracy)
--- HeartDisease_FlyAI/model.py
+++ HeartDisease_FlyAI/model.py
@@ -3,6 +3,7 @@ import numpy
 import os
 import torch
 from flyai.model.base import Base
+import xgboost as xgb
 
 from path import MODEL_PATH
 
@@ -12,16 +13,55 @@ Torch_MODEL_NAME = "model.pkl"
 
 
 class Model(Base):
-    def __init__(self, xgbClassifier):
-        self.xgbClassifier = xgbClassifier
+    # def __init__(self, xgbClassifier):
+    #     self.xgbClassifier = xgbClassifier
+    #
+    # def predict(self, **data):
+    #     prediction = self.xgbClassifier(data['dtest'])
+    #     return prediction
+    #
+    # def predict_all(self, datas):
+    #     return self.predict(datas)
+    #
+    # def save_model(self, model, path, name=Torch_MODEL_NAME, overwrite=False):
+    #     super().save_model(model, path, name, overwrite)
+    #     model.save_model(os.path.join(path, name))
+    #
+
+    def __init__(self, data):
+        self.data = data
+        self.xgbcls = xgb.Booster()
+        self.xgbcls.load_model(os.path.join(MODEL_PATH, Torch_MODEL_NAME))
 
     def predict(self, **data):
-        prediction = self.xgbClassifier(data['dtest'])
+        x_data = self.data.predict_data(**data)
+        x_data = xgb.DMatrix(x_data)
+        outputs = self.xgbcls.predict(x_data)
+        prediction = self.data.to_categorys(outputs)
         return prediction
 
     def predict_all(self, datas):
-        return self.predict(datas)
+        labels = []
+        for data in datas:
+            prediction = self.predict(**data)
+            labels.append(prediction)
+        return labels
 
-    def save_model(self, model, path, name=Torch_MODEL_NAME, overwrite=False):
-        super().save_model(model, path, name, overwrite)
-        model.save_model(os.path.join(path, name))
+    def batch_iter(self, x, y, batch_size=128):
+        """生成批次数据"""
+        data_len = len(x)
+        num_batch = int((data_len - 1) / batch_size) + 1
+
+        indices = numpy.random.permutation(numpy.arange(data_len))
+        x_shuffle = x[indices]
+        y_shuffle = y[indices]
+
+        for i in range(num_batch):
+            start_id = i * batch_size
+            end_id = min((i + 1) * batch_size, data_len)
+            yield x_shuffle[start_id:end_id], y_shuffle[start_id:end_id]
+
+    def save_model(self, xgbcls, path, name=Torch_MODEL_NAME, overwrite=False):
+        super().save_model(xgbcls, path, name, overwrite)
+        xgbcls.save_model(os.path.join(path, name))
+
